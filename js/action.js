cebaad5bbf1ddf9ebbfcf600aef04b09bb7da875
unknown <Greyson@Win8ProRTMDev.(none)>
2013-03-29 20:52:34 -0400

    Added templates for action and anim

---
+++ js/action.js
@@ -0,0 +1,15 @@
+this.tgd = this.tgd || {};
+
+(function()
+{
+    // Namespace var
+    var action = {};
+
+    action.test = function()
+    {
+        
+    }
+
+    // Copy back
+    this.tgd.action = action;
+})();
---
+++ js/anim.js
@@ -0,0 +1,15 @@
+this.tgd = this.tgd || {};
+
+(function()
+{
+    // Namespace var
+    var anim = {};
+
+    anim.test = function()
+    {
+        
+    }
+
+    // Copy back
+    this.tgd.anim = anim;
+})();
