fb02812f577522798242736bd2f112219fa862fd
Ben Chen <benshuyichen@gmail.com>
2013-03-29 22:01:43 -0400

    Small fixes

--- js/timer.js
+++ js/timer.js
@@ -5,19 +5,22 @@ this.tgd = this.tgd || {};
   this.myVar = {};
   this.started;
   this.t;
-  this.domain = window.location.host;
+  this.domain;
   
   var timer = {};
   timer.init = function() {  
-	  var myVar = setInterval(function(){myTimer()},1000); //initialize timer
-	  var started = 1; //timer flag
-	  var t = 0; //set start time
+	  myVar = setInterval(function(){myTimer()},1000); //initialize timer
+	  started = 1; //timer flag
+	  t = 0; //set start time
+	  domain = window.location.host;
   }
+  
   function myTimer()
   {
     t = t + 1;
     document.getElementById("timer").innerHTML=t;
   }
+  
   timer.restartTimer = function()
   {
     if (!started)
@@ -26,19 +29,23 @@ this.tgd = this.tgd || {};
       started = 1;
     }
   }
+  
   timer.pauseTimer = function()
   {
     clearInterval(myVar);
     started = 0;
   }
+  
   timer.saveTime = function()
   {
     localStorage.setItem(domain, t); //saves to the database, key/value
     window.alert(domain + " : " + t); //dev
   }
+  
   timer.getTime = function()
   {
     window.alert(domain + " : " + localStorage.getItem(domain)); //dev
   }
+  
   this.tgd.timer = timer;
 })();
