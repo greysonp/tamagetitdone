e24696638eefe4a91cc297b51ad8ddcedca32d6a
Ben Chen <BenShuyiChen@gmail.com>
2013-03-30 01:51:05 -0400

    Updated timer and TGD
    - Timer now checks for focus
    - Added weak and strong hunger levels and timeouts
    - Added callback function for tgd

--- js/tgd.js
+++ js/tgd.js
@@ -1,12 +1,21 @@
 this.tgd = this.tgd || {};
 
-(function()
+(function ()
 {
     var main = {};
     this.stage = {};
     var FPS = 30;
 
-    main.init = function()
+    //Timer Maximums
+    this.weakMax = 300; //seconds until weak hunger
+    this.strongMax = 600; //strong hunger
+    var hungerLevel = 0; //hunger level (0 = not hungry, 1 = weak, 2 = strong)
+
+    //Clock Maximums (for sleeping)
+    this.bedtimeHour = 23; //11PM
+    this.wakeupHour = 8; //8AM
+
+    main.init = function ()
     {
         // Initialize createjs
         this.stage = new createjs.Stage("tgd");
@@ -16,18 +25,43 @@ this.tgd = this.tgd || {};
 
         // Initialize our modules
         main.anim.init();
+        main.timer.init(timerCallback);
 
-//        this.timer.init();
+        // Initialize
     }
 
-    main.tick = function()
+    function timerCallback()
+    {
+        main.log("Timer Callback Received.");
+        main.timer.saveTime();
+        var curTime = main.timer.getTime();
+        localStorage.setItem("hungerLevel", hungerLevel); // store hunger level
+
+        if (curTime < this.weakMax)
+        {
+            hungerLevel = 0;
+            //do idle animation
+        }
+        else if (curTime >= this.weakMax && curTime < this.strongMax)
+        {
+            hungerLevel = 1;
+            //do weak animation
+        }
+        else if (curTime >= this.strongMax)
+        {
+            hungerLevel = 2;
+            //do strong animation
+        }
+    }
+
+    main.tick = function ()
     {
         this.stage.update();
     }
 
-    main.log = function(msg, isError)
+    main.log = function (msg, isError)
     {
-        if(args.length == 1 || !isError)
+        if (arguments.length == 1 || !isError)
             console.info(msg);
         else
             console.error(msg);
--- js/timer.js
+++ js/timer.js
@@ -1,53 +1,75 @@
 this.tgd = this.tgd || {};
 
-(function()
+(function ()
 {
-  var tid = {};
-  var started = false;
-  var t = {};
-  var domain = "";
-  
-  var timer = {};
+    var tid = {};
+    var t = {};
+    var domain = "";
+    var started = false;
+    var isActive = false;
+    var timer = {};
+    var cb = {};
 
-  timer.init = function()
-  {
-      tid = setInterval(function(){myTimer()},1000); //initialize timer
-	  started = 1; //timer flag
-	  t = 0; //set start time
-	  domain = window.location.host;
-  }
-  
-  function myTimer()
-  {
-      t = t + 1;
-      document.getElementById("timer").innerHTML=t;
-  }
-  
-  timer.restartTimer = function()
-  {
-      if (!started)
-      {
-          tid = setInterval(function(){myTimer()},1000);
-          started = 1;
-      }
-  }
-  
-  timer.pauseTimer = function()
-  {
-      clearInterval(tid);
-      started = 0;
-  }
-  
-  timer.saveTime = function()
-  {
-      localStorage.setItem(domain, t); //saves to the database, key/value
-      tgd.log(domain + " : " + t); //dev
-  }
-  
-  timer.getTime = function()
-  {
-      tgd.log(domain + " : " + localStorage.getItem(domain)); //dev
-  }
-  
-  this.tgd.timer = timer;
+    timer.init = function (callback)
+    {
+        cb = callback;
+        tid = setInterval(function ()
+        {
+            myTimer();
+            if (isActive)
+                cb();
+        }, 1000); //initialize timer
+        started = 1; //timer flag
+        t = 0; //set start time
+        domain = window.location.host;
+    }
+
+    function myTimer()
+    {
+        if (isActive)
+            t = t + 1;
+    }
+
+    timer.restartTimer = function (callback)
+    {
+        if (!started)
+        {
+            cb = callback;
+            tid = setInterval(function ()
+            {
+                myTimer();
+                cb();
+            }, 1000);
+            started = 1;
+        }
+    }
+
+    timer.pauseTimer = function ()
+    {
+        clearInterval(tid);
+        started = 0;
+    }
+
+    timer.saveTime = function ()
+    {
+        localStorage.setItem(domain, t); //saves to the database, key/value
+        tgd.log("SAVED: " + domain + " : " + t); //dev
+    }
+
+    timer.getTime = function ()
+    {
+        var curTime = localStorage.getItem(domain);
+        tgd.log("RETRIEVED: " + domain + " : " + curTime); //dev
+        return curTime;
+    }
+
+    window.onfocus = function () {
+        isActive = true;
+    };
+
+    window.onblur = function () {
+        isActive = false;
+    };
+
+    this.tgd.timer = timer;
 })();
